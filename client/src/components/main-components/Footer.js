e41f7a3ba275529ea956370389c172c3b8d7d66c
joseph <jphctolentino@gmail.com>
2020-03-14 11:50:57 +0000

    add: assests folder & logo

--- client/src/components/main-components/Footer.js
+++ client/src/components/main-components/Footer.js
@@ -4,7 +4,7 @@ import '../../styles/main-styles/footer.css';
 const Footer = ({ showPlayersNChat }) => {
 	return (
 		<div className="footer__container">
-			<div className="footer__player-list-toggle" onClick={e => { e.preventDefault(); showPlayersNChat() }}>
+			<div className="footer__player-list-toggle button" onClick={e => { e.preventDefault(); showPlayersNChat() }}>
 				Players & Chat
 			</div>
 		</div>
--- client/src/components/main-components/Navigation.js
+++ client/src/components/main-components/Navigation.js
@@ -1,12 +1,18 @@
 import React from 'react';
-import '../../styles/main-styles/navigation.css'
+import '../../styles/main-styles/navigation.css';
+import logo from '../../assets/images/logo.svg';
 
 const Navigation = () => {
 	return (
 		<div className="navigation__container">
-			<div className="navigation__logo">logo here</div>
+			<div className="navigation__logo">
+				<img src={logo} alt='logo' />
+				<p>
+					Vapour
+				</p>
+			</div>
 
-			<div className="navigation__logout">
+			<div className="navigation__logout button">
 				log out
 			</div>
 		</div>
