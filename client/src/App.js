821a3ef950fb93d8f2f7d8e2b0102e706a6569db
Pawel <pawel.pietruszka99@gmail.com>
2020-03-21 14:28:15 +0100

    add: forum, online user counting

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import "./styles/App.css";
+import io from 'socket.io-client';
 import { useSelector, useDispatch } from "react-redux";
 import { getCurrentUser } from "./redux/actions/current-user-actions";
 import Main from "./components/main-components/Main";
@@ -9,13 +10,21 @@ import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 import SocialMain from "./components/chat-players-components/SocialMain";
 import GameScreen from "./components/game-components/GameScreen";
 import Forum from "./components/chat-players-components/chat-components/Forum";
-
+let socket;
 function App() {
   const dispatch = useDispatch();
   const sessionUser = useSelector(state => state.session.user);
   const currentUser = useSelector(state => state.currentUser);
   useEffect(() => {
+    if (socket) {
+      socket.disconnect()
+    } else if (currentUser._id.length !== 0) {
+      console.log(currentUser)
+      socket = io('http://localhost:4000')
+      socket.emit('login', currentUser._id, () => {})
+    } 
     dispatch(getCurrentUser(sessionUser._id));
+    //delete logged in people
   }, [dispatch, sessionUser._id]);
 
   if (currentUser._id.length !== 0) {
@@ -25,7 +34,7 @@ function App() {
           <Navigation />
           <Switch>
             <Route exact path="/" component={Main} />
-            <Route exact path="/messages" component={SocialMain} />
+            <Route exact path="/messages" component={SocialMain} currentUser={currentUser} />
             <Route exact path="/game/:id">
               <GameScreen />
             </Route>
--- client/src/components/chat-players-components/chat-components/Forum.js
+++ client/src/components/chat-players-components/chat-components/Forum.js
@@ -1,21 +1,49 @@
-import React, { useState, useEffect } from "react";
-import MessageList from "./MessageList";
-import ChatForm from "./ChatForm";
-import "../../../styles/chat-styles/forum.css";
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+import MessageList from './MessageList';
+import ChatForm from './ChatForm';
+import { useSelector } from 'react-redux';
+// import "../../../styles/chat-styles/forum.css";
 
-const handleForumSubmit = message => {
-  console.log(message);
-};
+let socket;
+const ENDPOINT = 'http://localhost:4000/';
 
-const Forum = () => {
-  const [messages, setMessages] = useState([]);
+const Forum = ({ location }) => {
+  const [forumPosts, setForumPosts] = useState([]);
 
-  return (
-    <div className="forum__container">
-      <MessageList messages={messages} />
-      <ChatForm handleChatSubmit={handleForumSubmit} />
-    </div>
-  );
+  const currentUser = useSelector(({ currentUser }) => currentUser);
+
+  useEffect(() => {
+    socket = io(ENDPOINT);
+    if (currentUser.name.length !== 0) {
+      socket.emit('join', currentUser.name, 'forum', currentUser._id, () => {});
+    }
+    return () => {
+      socket.emit('disconnect');
+      socket.off();
+    };
+  }, [ENDPOINT, location]);
+
+  useEffect(() => {
+    socket.on('message', ({message}) => {
+      setForumPosts([...forumPosts, message]);
+    });
+  }, [forumPosts]);
+
+  const handleForumSubmit = message => {
+    if (message) {
+      socket.emit('message', {senderName: currentUser.name, message: message, time: new Date()}, () => {});
+    }
+  };
+
+  if (currentUser.name.length !== 0) {
+    return (
+      <div className="forum__container">
+        <MessageList messages={forumPosts} />
+        <ChatForm handleChatSubmit={handleForumSubmit} />
+      </div>
+    );
+  } else return null;
 };
 
 export default Forum;
