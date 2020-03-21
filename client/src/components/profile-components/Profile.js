14fa008942bafaee176f5e58998f16a1b84cf054
David B <david_beale@outlook.com>
2020-03-21 23:03:26 +0000

    profile: add try catch

--- client/src/components/profile-components/Profile.js
+++ client/src/components/profile-components/Profile.js
@@ -53,33 +53,37 @@ const Profile = () => {
     };
   }
   async function faceRecog (imageSrc) {
-    const image = new Image()
-    image.src=imageSrc;
-    const detections = await faceapi.detectAllFaces(image, new faceapi.TinyFaceDetectorOptions())
-    const x = detections[0].box["_x"];
-    const y = detections[0].box["_y"];
-    const width = detections[0].box["_width"];
-    const height = detections[0].box["_height"];
-    const canvas = document.createElement("canvas")
-    const ctx = canvas.getContext('2d');
-    canvas.width = image.width;
-    canvas.height = image.height;
-    const centerX = x + (width / 2)
-    const centerY = (y + (height / 2)) - (height * 0.2)
-    const radius = (height / 2) * 1.5
-    ctx.drawImage(image, 0, 0);
-    ctx.globalCompositeOperation = 'destination-in';
-    ctx.beginPath();
-    ctx.arc(centerX, centerY, radius, 0, Math.PI * 2);
-    ctx.closePath();
-    ctx.fill();
-    const tempCanvas = document.createElement("canvas")
-    const tCtx = tempCanvas.getContext("2d");
-    tempCanvas.width = radius*2;
-    tempCanvas.height = radius*2;
-    tCtx.drawImage(canvas, -(centerX-radius), -(centerY-radius));
-    const img = tempCanvas.toDataURL("image/png");
-    setImageSrc(img)
+    try {
+      const image = new Image()
+      image.src=imageSrc;
+      const detections = await faceapi.detectAllFaces(image, new faceapi.TinyFaceDetectorOptions())
+      const x = detections[0].box["_x"];
+      const y = detections[0].box["_y"];
+      const width = detections[0].box["_width"];
+      const height = detections[0].box["_height"];
+      const canvas = document.createElement("canvas")
+      const ctx = canvas.getContext('2d');
+      canvas.width = image.width;
+      canvas.height = image.height;
+      const centerX = x + (width / 2)
+      const centerY = (y + (height / 2)) - (height * 0.2)
+      const radius = (height / 2) * 1.5
+      ctx.drawImage(image, 0, 0);
+      ctx.globalCompositeOperation = 'destination-in';
+      ctx.beginPath();
+      ctx.arc(centerX, centerY, radius, 0, Math.PI * 2);
+      ctx.closePath();
+      ctx.fill();
+      const tempCanvas = document.createElement("canvas")
+      const tCtx = tempCanvas.getContext("2d");
+      tempCanvas.width = radius*2;
+      tempCanvas.height = radius*2;
+      tCtx.drawImage(canvas, -(centerX-radius), -(centerY-radius));
+      const img = tempCanvas.toDataURL("image/png");
+      setImageSrc(img)
+    } catch (error) {
+      alert('Sorry, your face was not detected. Please ensure your face is clearly visible')
+    }
   }
 
   if (currentUser.name.length !== 0) {
