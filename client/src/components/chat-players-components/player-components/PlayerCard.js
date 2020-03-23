00364a3f9bc57b3219659d64a8b0ea593d80ba0d
joseph <jphctolentino@gmail.com>
2020-03-23 14:33:43 +0000

    clean files

--- client/src/components/chat-players-components/player-components/PlayerCard.js
+++ client/src/components/chat-players-components/player-components/PlayerCard.js
@@ -1,64 +1,96 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import '../../../styles/player-styles/playercard.css';
-import chatIcon from '../../../assets/icons/chat-icon.png';
-import phoneIcon from '../../../assets/icons/phone-icon.png';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "../../../styles/player-styles/playercard.css";
+import chatIcon from "../../../assets/icons/chat-icon.png";
+import phoneIcon from "../../../assets/icons/phone-icon.png";
 // import { handleCreateCall } from '../../rtc-components/ToMerge-RtcClient';
 
-const PlayerCard = ({ player, handleShowChat, handleShowCall, calling, setCalling }) => {
-  const status = () => (player.status ? 'online' : 'offline');
+const PlayerCard = ({
+  player,
+  handleShowChat,
+  handleShowCall,
+  calling,
+  setCalling
+}) => {
+  const status = () => (player.status ? "online" : "offline");
   const [connected, setConnected] = useState(false);
   const statusButton = () =>
-    player.status ? 'button-enabled' : 'button-disabled';
+    player.status ? "button-enabled" : "button-disabled";
   return (
     <div className={`player-card__container ${status()}`}>
       <div className="player-card__player-name-container">
-        <h4 className="player-card__player-name">{player.name}</h4>
+        <p className="player-card__player-name">{player.name}</p>
       </div>
-      <div className="player-card__buttons-container"> <Link
-        className={`player-card__button ${statusButton()}`}
-        onClick={e => {
-          e.preventDefault();
-          player.status > 0 && handleShowChat(player);
-        }}
-        to={`/messages/?name=${player.name}`}
-      >
-        <img className="player-card__icon" src={chatIcon} alt="player-thumbnail"></img>
-      </Link>
+      <div className="player-card__buttons-container">
+        {" "}
+        <Link
+          className={`player-card__button ${statusButton()}`}
+          onClick={e => {
+            e.preventDefault();
+            player.status > 0 && handleShowChat(player);
+          }}
+          to={`/messages/?name=${player.name}`}
+        >
+          <img
+            className="player-card__icon"
+            src={chatIcon}
+            alt="player-thumbnail"
+          ></img>
+        </Link>
         <div
           className={`player-card__button call`}
-          //should create call request 
+          //should create call request
           onClick={e => {
             e.preventDefault();
             setCalling(true);
             handleShowCall(player);
           }}
         >
-          <img className="player-card__icon" src={phoneIcon} alt="player-thumbnail" />
+          <img
+            className="player-card__icon"
+            src={phoneIcon}
+            alt="player-thumbnail"
+          />
         </div>
-        {calling &&
+        {calling && (
           <div
-            //should have an event listener for incoming calls.  
+            //should have an event listener for incoming calls.
             class="player-card__call-buttons"
           >
             <div
               //should accept connection
-              onClick={e => { e.preventDefault(); setConnected(true); }}
-              className={`player-card__button pickup`}>
+              onClick={e => {
+                e.preventDefault();
+                setConnected(true);
+              }}
+              className={`player-card__button pickup`}
+            >
               pickup
-        </div>
+            </div>
             <div
               //should close connection
-              onClick={e => { e.preventDefault(); setCalling(false) }}
-              className={'player-card__button reject'}>
+              onClick={e => {
+                e.preventDefault();
+                setCalling(false);
+              }}
+              className={"player-card__button reject"}
+            >
               reject
-        </div>
+            </div>
           </div>
-        }
-        {connected && <div
-          //should close connection
-          onClick={e => { e.preventDefault(); setConnected(false); setCalling(false) }}
-        >end call</div>}
+        )}
+        {connected && (
+          <div
+            //should close connection
+            onClick={e => {
+              e.preventDefault();
+              setConnected(false);
+              setCalling(false);
+            }}
+          >
+            end call
+          </div>
+        )}
       </div>
     </div>
   );
